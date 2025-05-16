50a0dce79d3f193eae6cd74a89a671999a07d020
Ilia Krugliak <iliakrugliak97@gmail.com>
2025-05-16 22:25:40 +0300

    добавлена информация профиля Telegram

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import dynamic from "next/dynamic";
